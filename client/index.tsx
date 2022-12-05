42f56d9ddf3df924f63a2ebc7424d6d251ac823b
Luisortzg <88999324+Luisortzg@users.noreply.github.com>
2022-12-05 16:17:01 -0500

    WL Test

--- client/index.tsx
+++ client/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom';
-import { createRoot, Root } from 'react-dom/client';
+// import { createRoot, Root } from 'react-dom/client';
 const App = () => {
     return (
         <div>hello i am working</div>
