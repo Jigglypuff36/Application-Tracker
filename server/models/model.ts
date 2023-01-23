4bb0c0e9c62322082f15c3ed84e58c9cb3f4e89d
Luisortzg <88999324+Luisortzg@users.noreply.github.com>
2023-01-23 14:51:08 -0500

    Update model.ts

--- server/models/model.ts
+++ server/models/model.ts
@@ -1,7 +1,7 @@
 import {Pool} from 'pg';
 require('dotenv').config();
 
-const PG_URI: any = 'postgres://hcbdsxtj:QNIf6lS3Q3k_RPZXcfd6xLLcYUTK3Pe3@peanut.db.elephantsql.com/hcbdsxtj';
+const PG_URI: any = '';
 
 const pool = new Pool({
     connectionString: PG_URI
